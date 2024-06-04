c0f832b7984a1f1ac9261c3e1f22cb87ad304aff
Shamshiyev Og'abek <shamshiyevogabek5555@gmail.com>
2024-06-04 16:18:08 +0500

    add

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ document
     var email = document.getElementById("email").value;
     var password = document.getElementById("password").value;
     if (password === "12345") {
-      window.location.href = "http://127.0.0.1:5501/Kundalik.com/index.html";
+      window.location.href = "/Kundalik.com/index.html";
     } else {
       document.getElementById("errorMessage").textContent =
         "Iltimos parolni qayta kiriting!";
