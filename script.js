eec7acf62238d39817f8c5abcbe335d49a109fad
Shamshiyev Og'abek <shamshiyevogabek5555@gmail.com>
2024-06-04 16:22:14 +0500

    moved files from Kundalik.uz to pages folder

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ document
     var email = document.getElementById("email").value;
     var password = document.getElementById("password").value;
     if (password === "12345") {
-      window.location.href = "/Kundalik.com/index.html";
+      window.location.href = "/pages/index.html";
     } else {
       document.getElementById("errorMessage").textContent =
         "Iltimos parolni qayta kiriting!";
